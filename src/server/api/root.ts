6a448d85cad47b79ca8805315ba56ecdc7d6631f
ABay23 <alejandrobay23@gmail.com>
2023-09-14 23:22:51 -0400

    Added Posts

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,7 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { postsRouter } from "./routers/posts";
 
-/**
+/*
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
@@ -10,5 +10,5 @@ export const appRouter = createTRPCRouter({
   posts: postsRouter,
 });
 
-// export type definition of API
+//* export type definition of API
 export type AppRouter = typeof appRouter;
